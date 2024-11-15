764181da7c99714246f0c258b3517d1a5e58ee2f
Jagadeshwaran P <148187521+jaga-Innovation@users.noreply.github.com>
2024-11-15 13:31:13 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// Default using lists
+// import React from 'react';
+// import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+// function App() {
+//   const Courses = [
+//     { id: 1, name: 'Angular Courses' },
+//     { id: 2, name: 'React Courses' },
+//     { id: 3, name: 'AWS Courses' },
+//     { id: 4, name: 'Redux Courses' },
+//     { id: 5, name: 'NodeJS Courses' },
+//     { id: 6, name: 'Express Courses' },
+//     { id: 7, name: 'MEAN Courses' },
+//     { id: 8, name: 'MERN Courses' }
+//   ];
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <h1>React JS List and Keys</h1>
+//         <ul>
+//           {Courses.map((data) => (
+//             <li key={data.id}>{data.name}</li>
+//           ))}
+//         </ul>
+//       </header>
+//     </div>
+//   );
+// }
 
-export default App
+// export default App;
+
+
+
+
+
+
+
+
