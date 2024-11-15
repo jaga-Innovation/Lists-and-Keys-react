5f661a18f5cb7d9431b88fed2f3bf357a27b7510
Jagadeshwaran P <148187521+jaga-Innovation@users.noreply.github.com>
2024-11-15 15:42:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -47,38 +47,93 @@
 // List Required a Key
 // Keys are a way to help React know how to efficiently update a list
 
-const Developers = [
-  {
-    name: "raju",
-    skills: [
-      { name: "Angular", type: "Frontend" },
-      { name: "ExpressJS", type: "Backend" },
-    ],
-  },
+// const Developers = [
+//   {
+//     name: "raju",
+//     skills: [
+//       { name: "Angular", type: "Frontend" },
+//       { name: "ExpressJS", type: "Backend" },
+//     ],
+//   },
 
-  {
-    name: "bhai",
-    skills: [
-      { name: "React", type: "Frontend" },
-      { name: "NodeJS", type: "Backend" },
-    ],
-  },
+//   {
+//     name: "bhai",
+//     skills: [
+//       { name: "React", type: "Frontend" },
+//       { name: "NodeJS", type: "Backend" },
+//     ],
+//   },
 
-  {
-    name: "jackie",
-    skills: [
-      { name: "VueJS", type: "Frontend" },
-      { name: "PHP", type: "Backend" },
-    ],
-  },
+//   {
+//     name: "jackie",
+//     skills: [
+//       { name: "VueJS", type: "Frontend" },
+//       { name: "PHP", type: "Backend" },
+//     ],
+//   },
 
-  {
-    name: "chan",
-    skills: [
-      { name: "Swift", type: "Frontend" },
-      { name: "Python", type: "Backend" },
-    ],
-  },
-];
+//   {
+//     name: "chan",
+//     skills: [
+//       { name: "Swift", type: "Frontend" },
+//       { name: "Python", type: "Backend" },
+//     ],
+//   },
+// ];
 
+import React from "react";
+import "./App.css";
 
+function App() {
+  const Developers = [
+    {
+      name: "raju",
+      skills: [
+        { name: "Angular", type: "Frontend" },
+        { name: "ExpressJS", type: "Backend" },
+      ],
+    },
+    {
+      name: "bhai",
+      skills: [
+        { name: "React", type: "Frontend" },
+        { name: "NodeJS", type: "Backend" },
+      ],
+    },
+    {
+      name: "jackie",
+      skills: [
+        { name: "VueJS", type: "Frontend" },
+        { name: "PHP", type: "Backend" },
+      ],
+    },
+    {
+      name: "chan",
+      skills: [
+        { name: "Swift", type: "Frontend" },
+        { name: "Python", type: "Backend" },
+      ],
+    },
+  ];
+
+  return (
+    <div className="App">
+      <h1>Developers Detail</h1>
+      {Developers.map((developer, index) => (
+        <div key={index}>
+          <h2>Name: {developer.name}</h2>
+          <h3>Skills:</h3>
+          <ul>
+            {developer.skills.map((skill, skillIndex) => (
+              <li key={skillIndex}>
+                {skill.type} - {skill.name}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
